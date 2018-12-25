0f7875552629562a26aeedd46c42eca5fc4f45a7
Zachary Hughes <zhughes@manh.com>
2018-12-25 18:01:17 -0500

    moved truck and cargo methods to their own files

--- models/cargo.go
+++ models/cargo.go
@@ -1,13 +1,52 @@
 package models
 
+import (
+	"encoding/csv"
+	"io"
+	"log"
+	"os"
+	"strconv"
+)
+
 type Cargo struct {
-	Product string `json:"product"`
-	SrcCity string `json:"origin_city"`
-	SrcState string `json:"origin_state"`
-	SrcLat float64 `json:"origin_lat"`
-	SrcLng float64 `json:"origin_lng"`
-	DstCity string `json:"destination_city"`
-	DstState string `json:"destination_state"`
-	DstLat float64 `json:"destination_lat"`
-	DstLng float64 `json:"destination_lng"`
+	Product  string  `json:"product"`
+	SrcCity  string  `json:"origin_city"`
+	SrcState string  `json:"origin_state"`
+	SrcLat   float64 `json:"origin_lat"`
+	SrcLng   float64 `json:"origin_lng"`
+	DstCity  string  `json:"destination_city"`
+	DstState string  `json:"destination_state"`
+	DstLat   float64 `json:"destination_lat"`
+	DstLng   float64 `json:"destination_lng"`
+}
+
+func ParseCargos(filename string) []Cargo {
+	var cargos []Cargo
+	f, _ := os.Open(filename)
+	defer f.Close()
+	r := csv.NewReader(f)
+	for {
+		record, err := r.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+		//skip header record of csv
+		if record[0] != "product" {
+			cargos = createCargoFromRecord(cargos, record)
+		}
+	}
+
+	return cargos
+}
+
+func createCargoFromRecord(cargos []Cargo, record []string) []Cargo {
+	srcLat, _ := strconv.ParseFloat(record[3], 64)
+	srcLng, _ := strconv.ParseFloat(record[4], 64)
+	destLat, _ := strconv.ParseFloat(record[7], 64)
+	destLng, _ := strconv.ParseFloat(record[8], 64)
+	newCargo := Cargo{record[0], record[1], record[2], srcLat, srcLng, record[5], record[6], destLat, destLng}
+	return append(cargos, newCargo)
 }
--- models/truck.go
+++ models/truck.go
@@ -1,9 +1,46 @@
 package models
 
+import (
+	"encoding/csv"
+	"io"
+	"log"
+	"os"
+	"strconv"
+)
+
 type Truck struct {
-	Truck string `json:"truck"`
-	City string `json:"city"`
-	State string `json:"state"`
-	Lat float64 `json:"lat"`
-	Lng float64 `json:"lng"`
+	Truck string  `json:"truck"`
+	City  string  `json:"city"`
+	State string  `json:"state"`
+	Lat   float64 `json:"lat"`
+	Lng   float64 `json:"lng"`
+}
+
+func ParseTrucks(filename string) []Truck {
+	var trucks []Truck
+	f, _ := os.Open(filename)
+	defer f.Close()
+	r := csv.NewReader(f)
+	for {
+		record, err := r.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+		// skip header record of csv
+		if record[0] != "truck" {
+			trucks = createTruckFromRecord(trucks, record)
+		}
+	}
+
+	return trucks
+}
+
+func createTruckFromRecord(trucks []Truck, record []string) []Truck {
+	lat, _ := strconv.ParseFloat(record[3], 64)
+	lng, _ := strconv.ParseFloat(record[4], 64)
+	newTruck := Truck{record[0], record[1], record[2], lat, lng}
+	return append(trucks, newTruck)
 }
